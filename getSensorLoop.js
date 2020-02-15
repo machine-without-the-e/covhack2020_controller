c64935ec600c7028ac9261cab0db22bfa7c50876
Lewis Lovette <lewislovette3@hotmail.co.uk>
2020-02-15 21:52:35 +0000

    loop empty functions created

---
+++ getSensorLoop.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//functions for getting data
+exports.getMoistData = async ()=> {
+    //get data
+}
+
+exports.getLightData = async ()=> {
+    //get data
+}
+
+//functions for setting stuff data
+
+exports.setMoist = async ()=> {
+    //set data
+}
