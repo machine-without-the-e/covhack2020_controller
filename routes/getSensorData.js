b24b5267fcf7159703b715ca9936d53fc7409852
Lewis Lovette <lewislovette3@hotmail.co.uk>
2020-02-15 21:29:37 +0000

    required module in index

--- routes/getSensorData.js
+++ routes/getSensorData.js
@@ -1,1 +1,17 @@
 var Router = require('koa-router');
+
+var router = Router({
+    prefix: '/getSensorData'
+ }); 
+ 
+ //because we are going to parse POST parameters we will import koa-bodyparser
+ var bodyParser = require('koa-bodyparser');
+ 
+ router.post('/create_db', async (ctx, next) => {
+ 
+     let item = await adminModel.createTables(ctx.params.id);
+     ctx.body = item;
+ 
+ });
+ 
+ module.exports = router;
